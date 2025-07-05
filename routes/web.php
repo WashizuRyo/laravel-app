86119fe52028653ce4957e905958e0e665039c4f
Ryo Washizu <wasizuryo@gmail.com>
2025-07-05 15:48:22 +0900

    tagへのルートを作成

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TagController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('tasks', TaskController::class);
+Route::resource('tags', TagController::class);
