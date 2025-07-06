7c8fb4f9cf671edea7dc46b3cf8b0c17773f6678
Ryo Washizu <wasizuryo@gmail.com>
2025-07-06 19:57:10 +0900

    breezeを導入

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TagController;
@@ -8,7 +9,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::resource('tasks', TaskController::class);
 
 Route::delete('/tags/cleanup', [TagController::class, 'cleanup'])->name('tags.cleanup');
 Route::resource('tags', TagController::class);
+
+require __DIR__.'/auth.php';
