f6460e2326d8766b9d7ea7a8995097b076219aee
Ryo Washizu <wasizuryo@gmail.com>
2025-07-06 19:08:10 +0900

    old関数の第二引数にタスクのタイトルを設定. 初回閲覧時にインプットにタスクのタイトルが入るように

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -30,7 +30,7 @@
       <input type="text" 
              name="title" 
              id="title"
-             value="{{ old('title') }}" 
+             value="{{ old('title', $task->title) }}" 
              class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
     </div>
 
